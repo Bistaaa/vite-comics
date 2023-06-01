c964eb11a6132b8d0c212ec71e028fbf8abe3bfc
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-06-01 17:11:20 +0200

    sign up

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,6 +5,7 @@ export default {
 </script>
 
 <template>
+    <!-- STORE -->
     <section id="store">
         <div class="store-images-container">
             <img class="store-images" src="../assets/images/buy-comics-digital-comics.png">
@@ -23,13 +24,31 @@ export default {
             <p class="store-name">COMIC SHOP LOCATOR</p>
         </div>
         <div class="store-images-container">
-            <img class="store-images" src="../assets/images/buy-dc-power-visa.svg">
+            <img class="store-images" id="power-visa-img" src="../assets/images/buy-dc-power-visa.svg">
             <p class="store-name">DC POWER VISA</p>
         </div>
 
     </section>
+
+
+    <!-- LINK -->
+
     <section id="link"></section>
-    <section id="follow-us"></section>
+
+
+    <!-- FOLLOW US -->
+
+    <section id="follow-us">
+        <div id="follow-us-left">SIGN-UP NOW!</div>
+        <div id="follow-us-right">
+            <p id="follow-us-text">FOLLOW US</p>
+            <img class="social-logo" src="../assets/images/footer-facebook.png">
+            <img class="social-logo" src="../assets/images/footer-twitter.png">
+            <img class="social-logo" src="../assets/images/footer-youtube.png">
+            <img class="social-logo" src="../assets/images/footer-pinterest.png">
+            <img class="social-logo" src="../assets/images/footer-periscope.png">
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
@@ -48,11 +67,15 @@ export default {
 
         @include my-flex-row;
         align-items: center;
-        gap: 20px;
+        gap: 15px;
 
         .store-images {
             color: white;
             height: 60px;
+        }
+
+        #power-visa-img {
+            width: 60px;
         }
 
         .store-name {
@@ -61,5 +84,35 @@ export default {
         }
     }
 
+}
+
+#follow-us {
+    @include my-flex-row;
+    justify-content: space-between;
+    padding: 30px 15%;
+    background-color: rgba(48, 48, 48, 255);
+
+    #follow-us-left {
+        color: white;
+        padding: 13px;
+        border: 2px solid rgba(2, 130, 249, 255);
+        font-size: 18px;
+    }
+
+    #follow-us-right {
+        @include my-flex-row;
+        align-items: center;
+        gap: 20px;
+
+        #follow-us-text {
+            color: rgba(2, 130, 249, 255);
+            font-size: 22px;
+            font-weight: 600;
+        }
+
+        .social-logo {
+            height: 35px;
+        }
+    }
 }
 </style>
